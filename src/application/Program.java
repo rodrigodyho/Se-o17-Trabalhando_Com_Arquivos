d3914847e725022e13f0b57f62669708d61acc24
Rodrigo Lima <rodrigodyho@gmail.com>
2020-04-12 02:11:48 -0300

    FileWriter e BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,25 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-
 
 public class Program {
 
     public static void main(String[] args) {
-        String path = "C:\\Users\\Rodrigo Lima\\Documents\\temp\\int.txt";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line = br.readLine();
+        String[] lines = new String[] {"Good Morning", "Good Afternoon", "Good Night"};
 
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
+        String path = "C:\\Users\\Rodrigo Lima\\Documents\\temp\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            for (String line : lines){
+                bw.write(line);
+                bw.newLine();
             }
         }
         catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
