077cf60d6a651288c17d1fcc1e8841ebfab616c6
Rodrigo Lima <rodrigodyho@gmail.com>
2020-04-12 01:53:51 -0300

    FileReader e BufferReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,44 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 public class Program {
 
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\Rodrigo Lima\\Documents\\temp");
-        Scanner sc = null;
+        String path = "C:\\Users\\Rodrigo Lima\\Documents\\temp\\int.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
         try {
-            sc = new Scanner(file);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextInt());
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
         }
         catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
         finally {
-            if (sc != null) {
-                sc.close();
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
             }
         }
-
     }
+
 }
